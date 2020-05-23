7af3b2615cdf358af855898c6658cfd1fd0d0d68
Andy Cho <hello@andycho.ca>
2020-05-23 03:02:48 -0700

    Move a sphere around with head direction
    Can move a sphere around the screen to point to a specific place on the
    camera given the nose position and the direction of the head. This
    should give us a better way to check if the head is hitting something
    for the sound.

--- scripts/drumhead.js
+++ scripts/drumhead.js
@@ -1,3 +1,4 @@
+const Reactive = require('Reactive');
 const Scene = require('Scene');
 const Audio = require('Audio');
 export const FaceTracking = require('FaceTracking');
@@ -11,12 +12,17 @@ const FaceDirection = Object.freeze({
     BOTTOM_RIGHT: 4,
 });
 
+const camera = Scene.root.find('Camera');
+const focalPlane = camera.focalPlane;
+
+const sphere = Scene.root.find('Sphere');
+
 const playbackController =
 Audio.getPlaybackController('aircanPlaybackController');
 
 // Face rotation tracking - in radians
-let faceX = 0; // Left is negative, right is positive
-let faceY = 0; // Down is negative, up is positive
+let faceX = 0; // Left is negative, right is positive, [-PI, PI]
+let faceY = 0; // Down is negative, up is positive, [-PI/2, PI/2]
 const threshold = 0.1;
 let direction = FaceDirection.NONE;
 
@@ -78,6 +84,32 @@ FaceTracking.face(0).cameraTransform.rotationY.monitor().subscribe(function (eve
     update();
 });
 
-Diagnostics.watch('Face rotation horizontal ', FaceTracking.face(0).cameraTransform.rotationY);
-Diagnostics.watch('Face rotation veritcal ', FaceTracking.face(0).cameraTransform.rotationX);
-Diagnostics.watch('Face rotation z ', FaceTracking.face(0).cameraTransform.rotationZ);
+/**
+ * This gives the nose's coordinates from the perspective of the camera, where the camera
+ * is at (0,0,0).
+ * If you plop a 3D object in the scene and set the position to the position reported here,
+ * the object would be where the nose tip is.
+ */
+const noseTip = FaceTracking.face(0).cameraTransform.applyTo(FaceTracking.face(0).nose.tip);
+
+/**
+ * Trying to figure out how to rotate back from the position of the nose.
+ * Since the position of the nose is relative to the camera at (0,0,0), it's also the direction
+ * vector to the nose from the camera.
+ */
+const noseVector = Reactive.vector(noseTip.x, noseTip.y, noseTip.z);
+// This is the direction vector back from the nose to the camera
+const directionBackFromNose = Reactive.mul(noseVector, -1);
+// This should be the direction vector from the nose, transformed to the face rotation
+const transformedNoseDirection = FaceTracking.face(0).cameraTransform.applyTo(directionBackFromNose);
+// Making the direction vector a bit smaller since otherwise we'd reach all the way back to the camera's z-axis
+// This is too close to the camera so the sphere would not show up
+const slightlySmallerDirectionVector = Reactive.mul(transformedNoseDirection, 0.8);
+// Adding the location of the nose to the transformed direction should give a vector back from the nose
+const transformedNose = Reactive.add(slightlySmallerDirectionVector, noseVector);
+Diagnostics.watch('TNose x ', transformedNose.x);
+Diagnostics.watch('TNose y ', transformedNose.y);
+Diagnostics.watch('TNose z ', transformedNose.z);
+sphere.transform.x = transformedNose.x;
+sphere.transform.y = transformedNose.y;
+sphere.transform.z = transformedNose.z;
